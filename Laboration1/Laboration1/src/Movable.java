131b53eab6eefa6861e4a5256af3bd3ba7f741ae
GIFF <Per_a99@hotmail.com>
2018-11-15 14:26:38 +0100

    andra commit

---
+++ Laboration1/Laboration1/src/Movable.java
@@ -0,0 +1,14 @@
+/**
+ * Interface used for classes which can be moved and turned.
+ */
+
+public interface Movable {
+
+    void move();
+
+    void turnLeft();
+
+    void turnRight();
+
+
+}
---
+++ Laboration1/Laboration1/test/Volvo240Test.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class saabtest{
+    Saab95 saab95 = new Saab95(Color.BLUE, 20, 4,"95", Car.Direction.NORTH,100,100,true);
+
+    @Test
+    public void Turbo(){
+
+        saab95.startEngine();
+        saab95.setTurboOff();
+
+        assertEquals(0.2, saab95.speedFactor());
+
+        saab95.setTurboOn();
+
+        assertEquals(0.26, saab95.speedFactor());
+
+
+
+    }
+
+
+
+}
+class Volvo240Test {
+    Volvo240 volvo = new Volvo240(Color.BLUE, 20, 4, "240", Car.Direction.NORTH, 100, 100);
+
+    @Test
+    public void settersGetters() {
+        volvo.setColor(Color.YELLOW);
+        assertEquals(Color.YELLOW, volvo.getColor());
+        assertEquals(20, volvo.getEnginePower());
+        assertEquals(4, volvo.getNrDoors());
+        assertEquals("240", volvo.getModelName());
+        assertEquals(100, volvo.getX());
+        assertEquals(100, volvo.getY());
+
+    }
+
+    @Test
+    public void movement() {
+        volvo.startEngine();
+        volvo.turnLeft();
+        assertEquals(Car.Direction.WEST, volvo.getDir());
+
+        volvo.turnRight();
+        assertEquals(Car.Direction.NORTH, volvo.getDir());
+
+        volvo.move();
+        assertEquals(99, volvo.getY());
+
+    }
+
+    @Test
+    public void speed() {
+        volvo.startEngine();
+        assertEquals(0.1, volvo.getCurrentSpeed());
+
+        volvo.gas(0.5);
+        assertEquals(0.225, volvo.getCurrentSpeed());
+
+        volvo.brake(0.5);
+        assertEquals(0.1, volvo.getCurrentSpeed());
+
+        volvo.stopEngine();
+        assertEquals(0, volvo.getCurrentSpeed());
+    }
+}
